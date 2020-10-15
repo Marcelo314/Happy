f876450d92868ff57b7b0a18c93ac38fe986afc0
Marcelo Santos <henrique2013marcelo@gmail.com>
2020-10-15 03:14:01 -0300

    configurando mapas
    adicionando cordenadas ao mapa.
    criando popup icon e popup overlay.
    criando e adicionando marcador no mapa.

--- public/scripts/page-orphanages.js
+++ public/scripts/page-orphanages.js
@@ -1,12 +1,30 @@
-const map = L.map('mapid').setView([51.505, -0.09], 13);
+//criando mapa
+const map = L.map("mapid").setView([-27.222633, -49.6455874], 15);
 
-L.tileLayer(
-    'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', 
-{
-    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-}
-).addTo(map);
+//criando e add tilelayer
+L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
+  attribution:
+    '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+}).addTo(map);
 
-L.marker([51.5, -0.09]).addTo(map)
-    .bindPopup('A pretty CSS3 popup.<br> Easily customizable.')
-    .openPopup();
+//criando icone
+const icon = L.icon({
+    iconUrl: "./public/images/map-marker.svg",
+    iconSize: [58, 68],
+    iconAnchor: [29, 68],
+    popupAnchor: [170, 2]
+
+})
+
+//create popup overlay
+const popup = L.popup({
+    closeButton: false,
+    className: 'map-popup',
+    minWhidth: 240,
+    minHeight: 240
+}).setContent('Lar das meninas <a href="orphanage.html?id=1" class="choose-orphanage"> <img src="./public/images/arrow-white.svg" </a>')
+
+//create and add marker
+L.marker([-27.222633, -49.6455874], { icon })
+  .addTo(map)
+  .bindPopup(popup)
