d8306d74d3c5a4e361f68245d2bfd5d1eaee1f5e
Marcelo Santos <henrique2013marcelo@gmail.com>
2020-10-18 01:51:22 -0300

    configurando back-end

--- src/server.js
+++ src/server.js
@@ -1,1 +1,19 @@
-console.log('hello wolrd')
+// importar dependencia
+const express = require('express');
+const path = require('path')
+
+//iniciando o express
+const server = express()
+server
+//utilizando os arquivos estaticos
+    .use(express.static('public'))
+//configurar template engine
+    .set('views', path.join(__dirbane, "views"))
+    .set('view engine', 'hbs')
+// criar um rota
+    .get('/', (request, response) => {
+    return response.render('index')
+})
+
+// ligar o servidor
+server.listen(5500)
