78a788ee737cc163d18b86b035060947c9ff8773
Marcelo Santos <henrique2013marcelo@gmail.com>
2020-10-18 20:58:56 -0300

    teste database

--- src/database/fakedata.js
+++ src/database/fakedata.js
@@ -20,7 +20,7 @@ module.exports = [
         lat: "-27.222633",
         lng:  "-49.6555874",
         name: "Lar dos meninos", 
-        description: " Presta assistência a crianças de 06 a 15 anos que se encontre em situação de risco e/ou vulnerabilidade social.",
+        description: " presta assistência a crianças de 06 a 15 anos que se encontre em situação de risco e/ou vulnerabilidade social.",
         images: [
             "https://images.unsplash.com/photo-1600711724564-526eda91ac29?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1080&fit=max&ixid=eyJhcHBfaWQiOjF9",
 
