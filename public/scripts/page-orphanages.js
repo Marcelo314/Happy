61f0c19c6459812c16e132cae5495a513db56d71
Marcelo Santos <henrique2013marcelo@gmail.com>
2020-10-18 05:13:40 -0300

    migração para backend
    migração das páginas para o back-end

--- public/scripts/page-orphanages.js
+++ public/scripts/page-orphanages.js
@@ -1,30 +1,46 @@
 //criando mapa
-const map = L.map("mapid").setView([-27.222633, -49.6455874], 15);
+const map = L.map("mapid").setView([-27.222633, -49.6455874], 15)
 
 //criando e add tilelayer
 L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
   attribution:
     '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
-}).addTo(map);
+}).addTo(map)
 
 //criando icone
 const icon = L.icon({
-    iconUrl: "./public/images/map-marker.svg",
-    iconSize: [58, 68],
-    iconAnchor: [29, 68],
-    popupAnchor: [170, 2]
-
+  iconUrl: "/images/map-marker.svg",
+  iconSize: [58, 68],
+  iconAnchor: [29, 68],
+  popupAnchor: [170, 2]
 })
 
-//create popup overlay
-const popup = L.popup({
+function addMarker({id, name, lat, lng}) {
+  //create popup overlay
+  const popup = L.popup({
     closeButton: false,
     className: 'map-popup',
     minWhidth: 240,
     minHeight: 240
-}).setContent('Lar das meninas <a href="orphanage.html?id=1" class="choose-orphanage"> <img src="./public/images/arrow-white.svg" </a>')
+  }).setContent(`${name} <a href="/orphanage?id=${id}"><img src="/images/arrow-white.svg" 
+  </a>`)
 
-//create and add marker
-L.marker([-27.222633, -49.6455874], { icon })
+  //create and add marker
+  L
+  .marker([lat, lng], { icon })
   .addTo(map)
   .bindPopup(popup)
+}
+
+const orphanagesSpan = document.querySelectorAll('.orphanages span')
+
+orphanagesSpan.forEach( span => {
+    const orphanage = {
+        id: span.dataset.id,
+        name: span.dataset.name,
+        lat: span.dataset.lat,
+        lng: span.dataset.lgn
+    }
+
+    addMarker(orphanage)
+})
--- src/server.js
+++ src/server.js
@@ -1,19 +1,20 @@
 // importar dependencia
 const express = require('express');
-const path = require('path')
-
+const path = require('path');
+const pages = require('./pages.js');
 //iniciando o express
 const server = express()
 server
 //utilizando os arquivos estaticos
     .use(express.static('public'))
 //configurar template engine
-    .set('views', path.join(__dirbane, "views"))
+    .set('views', path.join(__dirname, "views"))
     .set('view engine', 'hbs')
-// criar um rota
-    .get('/', (request, response) => {
-    return response.render('index')
-})
 
+    // criar rotas
+    .get('/', pages.index)
+    .get('/orphanage', pages.orphanage)
+    .get('/orphanages', pages.orphanages)
+    .get('/create-orphanage', pages.createOrphanage)
 // ligar o servidor
 server.listen(5500)
